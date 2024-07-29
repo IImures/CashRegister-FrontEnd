3d555741d771dffd58bc14742919b400578cf71f
Vladyslav Kalynchenko <vlad.kalinchenko215@gmail.com>
2024-07-29 22:54:57 +0200

    Feature: Added FindUs page

--- src/app/find-us/find-us.component.ts
+++ src/app/find-us/find-us.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {NavigationListComponent} from "../footer/navigation-list/navigation-list.component";
+import {GeneralInfoComponent} from "../footer/general-info/general-info.component";
+import {MediaLinksComponent} from "../footer/media-links/media-links.component";
 
 @Component({
   selector: 'app-find-us',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NavigationListComponent,
+    GeneralInfoComponent,
+    MediaLinksComponent
+  ],
   templateUrl: './find-us.component.html',
   styleUrl: './find-us.component.scss'
 })
