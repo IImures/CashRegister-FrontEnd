244e503366751080dbe76bd792f95de1407aec8d
Vladyslav Kalynchenko <vlad.kalinchenko215@gmail.com>
2024-07-31 22:28:37 +0200

    Feature: Updated sidebar so it is generated from a json

--- src/app/header/side-bar/side-bar.component.ts
+++ src/app/header/side-bar/side-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NgClass, NgOptimizedImage} from "@angular/common";
+import {NgClass, NgForOf, NgOptimizedImage} from "@angular/common";
 import {SidebarHeaderService} from "../sidebar-header.service";
 
 @Component({
@@ -7,16 +7,60 @@ import {SidebarHeaderService} from "../sidebar-header.service";
   standalone: true,
   imports: [
     NgOptimizedImage,
-    NgClass
+    NgClass,
+    NgForOf
   ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+
+  categoryList: CatalogList[] = [
+    {
+      mainCategoryName: "Фіскальне обладнання",
+      nameId: "printers1",
+      subCategories: [
+        {
+          subCategoryName: "Касові апарати",
+          subCategoryUrl: "1"
+        },
+        {
+          subCategoryName: "Фіскальні реєстратори",
+          subCategoryUrl: "2"
+        }
+      ]
+    },
+    {
+      mainCategoryName: "Лазерні принтери",
+      nameId: "terminals1",
+      subCategories: [
+        {
+          subCategoryName: "Ремонт лазерних принтерів",
+          subCategoryUrl: "3"
+        },
+        {
+          subCategoryName: "Заправка картриджів",
+          subCategoryUrl: "4"
+        }
+      ]
+    }
+  ];
+
   sidebarHeader: SidebarHeaderService
 
   constructor(sidebarHeader: SidebarHeaderService) {
     this.sidebarHeader = sidebarHeader;
   }
 
+}
+
+export interface CatalogList{
+  mainCategoryName: string;
+  nameId: string;
+  subCategories: SubCategory[];
+}
+
+export interface SubCategory{
+  subCategoryName: string;
+  subCategoryUrl: string;
 }
