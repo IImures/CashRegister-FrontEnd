cc97bdb84c536bbea305bfa3ee930112b4724cba
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-08-12 20:29:54 +0200

    Feature: Added Environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: "http://localhost:8080/api",
+  apiVersion: "v1",
+  hideHeaderOn: ['/login', '/register', '/error', '/edit'],
+  hideFooterOn: ['/login', '/register', '/error', '/edit']
+};
