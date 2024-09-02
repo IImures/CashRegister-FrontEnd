e57c8339068068689a85c3431ebf12cdb2d8cc71
Vladyslav Kalynchenko <vladyslav.kalynchenko@gmail.com>
2024-09-02 14:31:10 +0200

    Fix: Changed api for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://172.22.208.1:8080/api",
+  apiUrl: "http://88.198.127.218:8080/api",
   apiVersion: "v1",
   hideHeaderOn: ['/login', '/register', '/error', '/edit'],
   hideFooterOn: ['/login', '/register', '/error', '/edit']
